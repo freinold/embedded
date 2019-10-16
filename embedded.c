10177f99719e391944896bc3330925abe78c8783
freinold <fabian.reinold@web.de>
2019-10-16 17:55:13 +0200

    Finished implementing Exercise 1.

--- embedded.c
+++ embedded.c
@@ -1,7 +1,68 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+void* taskA( void* args);
+void* taskB( void* args);
 
 int main(int argc, char *argv[]) {
-	printf("Welcome to the QNX Momentics IDE\n");
+	pthread_attr_t attrA;
+	pthread_attr_t attrB;
+	pthread_t idA;
+	pthread_t idB;
+	int ret;
+	ret = pthread_attr_init( &attrA );
+	if (ret != 0) {
+		printf("pthread_attr_init: %s\n", strerror(ret));
+		exit(-1);
+	}
+	ret = pthread_attr_init( &attrB );
+	if (ret != 0) {
+		printf("pthread_attr_init: %s\n", strerror(ret));
+		exit(-1);
+	}
+	pthread_t returnA;
+	pthread_t returnB;
+	ret = pthread_create( &idA, &attrA, &taskA, NULL );
+	if (ret != 0) {
+		printf("pthread_create: %s\n", strerror(ret));
+		exit(-1);
+	}
+	ret = pthread_create( &idB, &attrB, &taskB, NULL );
+	if (ret != 0) {
+		printf("pthread_create: %s\n", strerror(ret));
+		exit(-1);
+	}
+	ret = pthread_join( idA, (void**) &returnA );
+	if (ret != 0) {
+		printf("pthread_join: %s\n", strerror(ret));
+		exit(-1);
+	}
+	ret = pthread_join( idB, (void**) &returnB );
+	if (ret != 0) {
+		printf("pthread_join: %s\n", strerror(ret));
+		exit(-1);
+	}
+	printf("ID A: %d / %d; ID B: %d / %d\n", idA, returnA, idB, returnB);
+	if (idA != returnA || idB != returnB) {
+		printf("Mismatched ID's, something's wrong...");
+		exit(-1);
+	}
 	return EXIT_SUCCESS;
+}
+
+void* taskA( void* args ) {
+	sleep(5);
+	pthread_t id = pthread_self();
+	printf("I'm thread A! ID = %d\n", id);
+	pthread_exit( (void*) id );
+}
+
+void* taskB( void* args ) {
+	sleep(2);
+	pthread_t id = pthread_self();
+	printf("I'm thread B! ID = %d\n", id);
+	pthread_exit( (void*) id );
 }
